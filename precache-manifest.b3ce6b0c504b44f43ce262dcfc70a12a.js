8ad40774172fa240b420260c45836bdba82d0810
Kitiya <kate.kitiya@gmail.com>
2020-03-22 22:03:45 -0400

    Updates

---
+++ precache-manifest.b3ce6b0c504b44f43ce262dcfc70a12a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910e34845e15a8b80c7173c1b0d79b22",
+    "url": "/github.io/covid19-tracker/index.html"
+  },
+  {
+    "revision": "f290cb99dcbc3141b4e3",
+    "url": "/github.io/covid19-tracker/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d255e8fb596eb174fd42",
+    "url": "/github.io/covid19-tracker/static/js/2.0721c362.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github.io/covid19-tracker/static/js/2.0721c362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f290cb99dcbc3141b4e3",
+    "url": "/github.io/covid19-tracker/static/js/main.fa1757f9.chunk.js"
+  },
+  {
+    "revision": "b510a550badcd4997ed0",
+    "url": "/github.io/covid19-tracker/static/js/runtime-main.dbd0b295.js"
+  }
+]);
